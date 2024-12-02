2833a2c963ca71a5043f6874e448dd124cc0ddba
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-12-02 12:15:51 +0100

    first commit

---
+++ lib/secrets.dart
@@ -0,0 +1,2 @@
+const openAIAPIKey =
+    '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';
